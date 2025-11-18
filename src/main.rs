bba4d39b0cdd67f55665f654ba6dcc684f2a68a1
Teradad41 <sitiang120@gmail.com>
2025-11-19 00:16:43 +0900

    healthCheck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use axum::http::StatusCode;
+use axum::{Router, routing::get};
+use std::net::{Ipv4Addr, SocketAddr};
+use tokio::net::TcpListener;
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await?;
+
+    println!("Listening on {}", addr);
+    Ok(axum::serve(listener, app).await?)
+}
+
+#[tokio::test]
+async fn health_check_test() {
+    let status_code = health_check().await;
+    assert_eq!(StatusCode::OK, status_code);
 }
